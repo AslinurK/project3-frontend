27dcc35c8f998ff3dec9d2ca046c19518f6b6a0f
Asli <aslinurkeklikci@yahoo.com>
2021-01-31 12:26:47 -0500

    change

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios  from 'axios';  
 
 const api = axios.create({
-    baseURL:'https://project3-api-frontend.herokuapp.com'
+    baseURL:'https://project3-api-frontend.herokuapp.com/'
 })
 
 export default api;
